9b6c4471a661f401dc0fefe7d72d184203e85771
mrrighto <38061236+mrrighto@users.noreply.github.com>
2018-06-19 14:04:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -607,7 +607,7 @@ bot.on("messageReactionAdd", (reaction, user) => {
         target1.addRole('432384823068459018').catch(console.error);
     }else if (reaction.message.channel.name == "welcome" && reaction.emoji.identifier == "%F0%9F%85%B1" && reaction.message.content.startsWith("**5.** **Choose wisely here:**")) {
         target1.addRole('432877618954764289').catch(console.error);
-      }else if (reaction.message.channel.name == "welcome" && reaction.emoji.identifier == "%E2%9C%85" && reaction.message.content == "**6.** **Merchant tag**, @rize#3801 will DM you specific items from the #marketplace, the items will be filtered based on what you did in this survey. __Press check if you want this feature__") {
+      }else if (reaction.message.channel.name == "welcome" && reaction.emoji.identifier == "%E2%9C%85" && reaction.message.content.startsWith("**6.** **Merchant tag**")) {
         target1.addRole('458508768175456268').catch(console.error);
     }
 
