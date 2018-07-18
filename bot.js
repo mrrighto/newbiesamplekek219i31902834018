0790edc7b003ee5ecd0b1418740cdca7c3c29804
mrrighto <38061236+mrrighto@users.noreply.github.com>
2018-07-18 18:11:57 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ function EMEDailycode() {
     let scheduledailyc = require('node-schedule');
     let ruledailyc = new schedule.RecurrenceRule();
     ruledailyc.hour = 17;
-    ruledailyc.minute = 15;
+    ruledailyc.minute = 5;
     let jdailyc = scheduledailyc.scheduleJob(ruledailyc, function () {
         
        try {
