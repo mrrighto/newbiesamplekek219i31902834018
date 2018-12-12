f2be6650586964d69d17c435b963c20fc3868c9c
mrrighto <38061236+mrrighto@users.noreply.github.com>
2018-12-13 03:44:27 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1727,16 +1727,25 @@ bot.on("messageReactionAdd", (reaction, user) => {
 
 
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.identifier == "1%E2%83%A3" && reaction.message.content.startsWith("1. **[ Notifications ] -**")) {
-        target1.addRole('432877618954764289').catch(console.error);
+        if (!target1.roles.has("432877618954764289") || target1.roles.has("432384823068459018") || target1.roles.has("522365368249876480") || target1.roles.has("521323904095223808") || target1.roles.has("432384941855080448")) return;
+	target1.send("You already selected #1, please read the instructions du*b*k");
+	target1.addRole('432877618954764289').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.identifier == "2%E2%83%A3" && reaction.message.content.startsWith("1. **[ Notifications ] -**")) {
-        target1.addRole('432384823068459018').catch(console.error);
+        if (target1.roles.has("432877618954764289") || !target1.roles.has("432384823068459018") || target1.roles.has("522365368249876480") || target1.roles.has("521323904095223808") || target1.roles.has("432384941855080448")) return;
+	target1.send("You already selected #2, please read the instructions du*b*k");       
+	 target1.addRole('432384823068459018').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.identifier == "3%E2%83%A3" && reaction.message.content.startsWith("1. **[ Notifications ] -**")) {
-        target1.addRole('522365368249876480').catch(console.error);
+                if (target1.roles.has("432877618954764289") || target1.roles.has("432384823068459018") || !target1.roles.has("522365368249876480") || target1.roles.has("521323904095223808") || target1.roles.has("432384941855080448")) return;
+	target1.send("You already selected #3, please read the instructions du*b*k");  
+	target1.addRole('522365368249876480').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.identifier == "4%E2%83%A3" && reaction.message.content.startsWith("1. **[ Notifications ] -**")) {
-        target1.addRole('521323904095223808').catch(console.error);
+        if (target1.roles.has("432877618954764289") || target1.roles.has("432384823068459018") || target1.roles.has("522365368249876480") || !target1.roles.has("521323904095223808") || target1.roles.has("432384941855080448")) return;
+	target1.send("You already selected #4, please read the instructions du*b*k");         
+ target1.addRole('521323904095223808').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.name == "ping" && reaction.message.content.startsWith("1. **[ Notifications ] -**")) {
-        target1.addRole('432384941855080448').catch(console.error);
-
+               if (target1.roles.has("432877618954764289") || target1.roles.has("432384823068459018") || target1.roles.has("522365368249876480") || target1.roles.has("521323904095223808") || !target1.roles.has("432384941855080448")) return;
+	target1.send("You already selected #5 (ping), please read the instructions du*b*k");   
+	target1.addRole('432384941855080448').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.identifier == "1%E2%83%A3" && reaction.message.content.startsWith("2. **[ Notifications ] -**")) {
         target1.addRole('516894630672072715').catch(console.error);
     }else if (reaction.message.channel.name == "bot-services🔒" && reaction.emoji.name == "ping" && reaction.message.content.startsWith("2. **[ Notifications ] -**")) {
